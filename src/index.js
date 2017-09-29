168c8552f19d48cd97692c7c8bb443b58f922dc6
jstrR <srovk22@gmail.com>
2017-09-29 21:24:24 +0300

    brackets 100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0) return false;
+    let arr = str.split('');
+    let buffer = [];
+    for (let i = 0; i < str.length; i++){
+        for (let j = 0; j < bracketsConfig.length; j++){
+            if (arr[i] === bracketsConfig[j][1] && buffer[buffer.length - 1] === bracketsConfig[j][0])
+                    buffer.pop();
+            else if (arr[i] === bracketsConfig[j][0])
+                buffer.push(arr[i]);
+        }
+    }
+    if (buffer.length === 0) {
+        return true;
+  	} 
+  	else { 
+        return false; 
+  	}
+
 }
